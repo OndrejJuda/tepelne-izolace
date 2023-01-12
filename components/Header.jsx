66036d5cb808e8b6e607338884cb74014f625a91
Ondrej Juda <ondrej.juda@networg.com>
2023-01-12 15:21:59 +0100

    header

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,16 +1,35 @@
 import React from 'react';
+import Link from 'next/link';
+
+const links = [
+  { id: '#kontakty', title: 'Kontakt' },
+  { id: '#benefity', title: 'Proč nás?' },
+  { id: '#služby', title: 'Služby' },
+  { id: '#proces', title: 'Proces' },
+  { id: '#reference', title: 'Reference' },
+];
 
 const Header = () => {
   return (
-    <header className='flex'>
-      <img src='/logo.png' alt='logo' width={64} height={64} />
-      <nav className='ml-auto'>
-        <ul className='flex'>
-          <li>Kontakt</li>
-          <li>Proč nás?</li>
-          <li>Služby</li>
-          <li>Proces</li>
-          <li>Reference</li>
+    <header
+      className='flex justify-around items-center p-8'
+    >
+      <img src='/logo-name.png' alt='logo' width={300} />
+      <nav className=''>
+        <ul className='flex justify-center items-center gap-12'>
+          {
+            links.map(({ id, title }) => (
+              <li
+                key={id}
+                className='text-primary-500 text-xl font-semibold group relative overflow-hidden'
+              >
+                <Link href={id}>
+                  {title}
+                </Link>
+                <div className='w-full h-[2px] bg-primary-500 -translate-x-[100%] group-hover:translate-x-0 transition' />
+              </li>
+            ))
+          }
         </ul>
       </nav>
     </header>

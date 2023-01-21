a381eb295d459aa83c204d50a1029c037d4c783f
Ondrej Juda <ondrej.juda@networg.com>
2023-01-21 18:07:23 +0100

    responsive process

--- components/Process.jsx
+++ components/Process.jsx
@@ -6,43 +6,43 @@ const processSteps = [
   { image: '/business/preparation.jpg', title: 'Příprava realizace', descripton: 'Ullamco adipisicing ullamco minim deserunt aliquip dolore nisi amet dolor ad ipsum.' },
   { image: '/business/construction.jpg', title: 'Realizace', descripton: 'Lorem sint sit magna ea duis.' },
   { image: '/business/details.jpg', title: 'Doladění detailů', descripton: 'Dolore cupidatat laboris cillum fugiat nulla dolore adipisicing.' },
-  { image: '/business/finish.jpg', title: 'Dokončení izolace', descripton: 'Sit quis laborum elit dolore veniam tempor ex nostrud dolore ullamco ipsum labore proident.' },
+  { image: '/business/finish.jpg', title: 'Dokončení izolace', descripton: 'Sit quis laborum elit dolore veniam tempor ex nostrud dolore ullamco ipsum labore proident.Sit quis laborum elit dolore veniam tempor ex nostrud dolore ullamco ipsum labore proident.' },
 ];
 
 const Process = () => {
   return (
     <section
       id='proces'
-      className='flex justify-center my-48 '
+      className='flex justify-center my-48'
     >
       <div className='flex flex-col my-36 justify-center items-center'>
-        <HSecondary additionalStyling='text-5xl mb-32'>Jak to u nás funguje?</HSecondary>
-        <ul className='flex flex-col gap-16 mb-32'>
+        <HSecondary additionalStyling='text-4xl md:text-5xl mb-32'>Jak to u nás funguje?</HSecondary>
+        <ul className='flex flex-col gap-24 xl:gap-16 mb-16 xl:mb-32 mx-8 lg:mx-16'>
           {
             processSteps.map(({ image, title, descripton }, index) => (
               <li
                 height={100}
                 key={title}
-                className='flex-1 flex flex-col md:flex-row gap-32'
+                className='xl:flex-1 flex flex-col xl:flex-row gap-12 xl:gap-32'
               >
-                <img
-                  src={image}
-                  alt={image}
-                  className={`${index % 2 ? 'order-1' : 'order-2'} w-[400px] h-[400px] object-cover rounded-xl flex-1 shadow-xl`}
-                />
-                <div className={`${index % 2 ? 'order-2' : 'order-1'} flex-1 flex flex-col justify-center`}>
+                <div className={`${index % 2 ? 'xl:order-2' : 'xl:order-1'} xl:flex-1 flex flex-col justify-center`}>
                   <div className='relative'>
                     <span
-                      className={`text-8xl text-white bg-primary-50 p-2 w-[164px] h-[164px] rounded-full opacity-50
-                    absolute -top-10 ${index % 2 ? '-right-28' : '-left-28'}
-                    flex justify-center items-center -z-10`}
+                      className={`text-8xl text-white bg-primary-50 p-2 w-[120px] h-[120px] xl:w-[164px] xl:h-[164px] rounded-full opacity-50
+                      absolute -top-10 right-0 ${index % 2 ? 'xl:-right-28' : 'xl:-left-28'}
+                      flex justify-center items-center -z-10`}
                     >
                       {index + 1}
                     </span>
                     <p className='text-3xl font-semibold text-primary-800 mb-8'>{title}</p>
                   </div>
-                  <p className='text-xl text-primary-800'>{descripton}</p>
+                  <p className='max-w-[800px] text-xl text-primary-800'>{descripton}</p>
                 </div>
+                <img
+                  src={image}
+                  alt={image}
+                  className={`${index % 2 ? 'xl:order-1' : 'xl:order-2'} xl:w-[400px] h-[400px] object-cover rounded-xl xl:flex-1 shadow-xl`}
+                />
               </li>
             ))
           }

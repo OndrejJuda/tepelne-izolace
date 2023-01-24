97572f5b680f8d9c7cb7e98939d1feed38b35339
Ondrej Juda <ondrej.juda@networg.com>
2023-01-24 15:39:36 +0100

    contact info

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -3,13 +3,13 @@ import { CTAButton } from './';
 
 const Hero = () => {
   return (
-    <section className='flex flex-col items-center bg-primary-50 py-16 lg:py-40 md:clip-hero overflow-hidden relative'>
+    <section className='flex flex-col items-center bg-primary-50 lg:clip-hero overflow-hidden relative'>
       <img
         src='/logo/logo-one-color.svg'
         alt='logo'
         className='absolute top-[600px] -right-44 md:right-32 md:top-64 opacity-50 scale-125'
       />
-      <div className='flex flex-col justify-center items-center lg:items-start p-6 sm:px-12 min-h-[calc(100vh-155px)] md:h-auto relative'>
+      <div className='flex flex-col justify-center items-center lg:items-start p-6 sm:px-12 min-h-[calc(100vh-110px)] sm:min-h-[calc(100vh-155px)] md:h-auto relative'>
 
         <h1 className='text-5xl md:text-6xl font-bold text-primary-600 mb-16'>
           Tepelné izolace snadno a rychle
@@ -19,7 +19,27 @@ const Hero = () => {
           Culpa duis excepteur eiusmod nulla elit aute Lorem nulla ex officia aute exercitation culpa reprehenderit.
         </p>
 
-        <CTAButton>Nezávazná poptávka</CTAButton>
+        <div className='lg:hidden flex flex-col items-center gap-4 bg-white py-6 px-6 rounded-xl'>
+          <a href='mailto:test@test.com' className='text-xl font-semibold text-primary-700'>test@test.com</a>
+          <CTAButton>Nezávazná poptávka</CTAButton>
+          <a href='tel:+420775689770' className='text-xl font-semibold text-primary-700'>+420 775 689 770</a>
+        </div>
+
+        <div className='hidden lg:flex items-center gap-8 bg-white rounded-full pr-8'>
+          <div className='scale-110'>
+          <CTAButton>Nezávazná poptávka</CTAButton>
+          </div>
+          <a href='mailto:test@test.com' className='text-xl font-semibold text-primary-700'>test@test.com</a>
+          <a href='tel:+420775689770' className='text-xl font-semibold text-primary-700'>+420 775 689 770</a>
+        </div>
+
+        {/* <div className='flex flex-col lg:flex-row lg:gap-8 items-center lg:bg-white lg:rounded-full'>
+          <a href='mailto:test@test.com' className='text-lg font-bold text-primary-900 p-2 px-6 pb-4 -mb-2 bg-white rounded-t-3xl'>test@test.com</a>
+          <div className='z-10'>
+            <CTAButton>Nezávazná poptávka</CTAButton>
+          </div>
+          <a href='tel:+420775689770' className='text-lg font-bold text-primary-900 p-2 px-6 pt-4 -mt-2 bg-white rounded-b-3xl'>+420 775 689 770</a>
+        </div> */}
       </div>
 
     </section>

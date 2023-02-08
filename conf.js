994cd865fc03730e42d9e490c3896f642e49dfac
Ondrej Juda <ondrej.juda@networg.com>
2023-02-08 23:28:02 +0100

    metadata description

--- conf.js
+++ conf.js
@@ -1,6 +1,6 @@
 const configuration = {
   title: 'LUNASTAV CZ s.r.o.',
-  description: 'Tepelné izolace do každé rodiny.',
+  description: 'Zateplujeme pomocí foukané izolace a vyřídíme za Vás dotaci NZÚ Light.',
   url: 'https://www.lunastav.cz',
   email: 'info@lunastav.cz',
   phone: '+420 605 898 894',

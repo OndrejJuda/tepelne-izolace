bd9e2d609978d0c169eda6d389133c63068d7266
Ondrej Juda <ondrej.juda@networg.com>
2023-02-08 21:52:40 +0100

    services

--- components/Services.jsx
+++ components/Services.jsx
@@ -7,18 +7,8 @@ const Services = () => {
       id='služby'
       innerDivStyle=''
     >
-      <div className='text-primary-100 text-lg md:text-3xl 
-      mx-8 md:mx-16 lg:mx-36 mb-36'>
-        <p className='mb-8'>
-          Zabýváme se zateplováním budov pomocí foukané izolace.
-          Věříme, že kvalitně zateplený dům je v současné situaci správnou cestou ke snížení nákladů Vaší domácnosti. Preferujeme kvalitu před kvantitou a zakládáme si na individuálním přístupu ke každému klientovi.
-        </p>
-        <p className=''>
-          Naší odborníci Vám pomohou vybrat idealní variantu zateplení pro Váš dům a vyřídíme Vám státní dotaci od NZÚ.
-        </p>
 
-      </div>
-      <div className='text-primary-100 text-3xl 
+      {/* <div className='text-primary-100 text-3xl 
       mx-8 md:mx-16 lg:mx-36 mb-36'>
         <p className='mb-8'>
           Vyřizujeme dotace z programu NZÚ (Light)
@@ -33,44 +23,69 @@ const Services = () => {
           <p className='mb-8'>
           odstavec
         </p>
-      </div>
+      </div> */}
 
-      <div className='mx-8 md:mx-16 lg:mx-36 bg-secondary-600 flex flex-col xl:flex-row rounded-lg overflow-hidden shadow-xl'>
-        <div
-          className='p-8 lg:p-20 
-          flex-1 flex flex-col justify-center order-2 xl:order-1'
-        >
-          <HSecondary additionalStyling='text-5xl mb-10'>Používáme čedičovou vatu, nebo kvalitní minerální vlnu vyráběnou v Belgii</HSecondary>
-          <div
-            className='text-primary-100 text-lg flex flex-col gap-4'
-          >
-            <p>
-              Foukaná minerální izolace je tepelná izolace, která se aplikuje na stěny, stropy a podhledy pomocí speciálního stroje, který rozptýlí vláknité materiály, jako jsou skleněné vlákna nebo minerální vlákna, do nástěnných prostorů. Foukananá izolace se díky svým vlastnostem poskytuje dokonalou tepelnou izolaci téměř každému objektu bez vzniku tepelných mostů. Tato izolace má několik výhod, jako jsou:
-            </p>
-            <p>
-              1. Vysoká tepelná izolace: foukaná minerální izolace poskytuje vysokou tepelnou izolaci, což pomáhá udržovat teplo uvnitř budovy a snižuje potřebu vytápění.
-            </p>
-            <p>
-              2. Snadná instalace: foukaná minerální izolace se snadno aplikuje pomocí speciálního stroje, což umožňuje rychlou a efektivní instalaci bez potřeby velkých stavebních úprav.
-            </p>
-            <p>
-              3. Flexibilita: Foukaná minerální izolace se dá aplikovat na různé typy konstrukcí a prostorů, včetně obtížně přístupných míst.
-            </p>
-            <p>
-              4. Odolnost: Foukaná minerální izolace je odolná proti vlhkosti a plísním, což ji činí vhodnou pro použití ve vlhkých prostorách, jako jsou koupelny nebo sklepy.
-            </p>
-            <p>
-              5. Bezpečnost: Foukaná minerální izolace je nehořlavá, bezpečná pro použití v interiéru a neobsahuje žádné škodlivé látky.
-            </p>
+      <div className='mx-8 md:mx-16 lg:mx-36 bg-secondary-600 rounded-lg overflow-hidden shadow-xl 2xl:flex'>
+        <div>
+
+          <div className='p-8 lg:p-20'>
+            <HSecondary additionalStyling='text-5xl mb-10'>Co Vám můžeme nabídnout</HSecondary>
+            <div className='text-primary-100 text-lg '>
+              <p className='mb-8'>
+                Zabýváme se zateplováním budov pomocí foukané izolace.
+                Věříme, že kvalitně zateplený dům je v současné situaci správnou cestou ke snížení nákladů Vaší domácnosti. Preferujeme kvalitu před kvantitou a zakládáme si na individuálním přístupu ke každému klientovi.
+              </p>
+              <p className=''>
+                Naší odborníci Vám pomohou vybrat idealní variantu zateplení pro Váš dům a vyřídíme Vám státní dotaci od NZÚ.
+              </p>
+            </div>
+          </div>
+
+          <img
+            src='/business/ursa.jpg'
+            alt='ursa'
+            className='block 2xl:hidden flex-1 object-cover w-full h-[400px]'
+          />
+
+          <div className='flex flex-col xl:flex-row'>
+            <div
+              className='p-8 lg:p-20 2xl:pt-0
+              flex-1 flex flex-col justify-center order-2'
+            >
+              <HSecondary additionalStyling='text-5xl mb-10'>Používáme čedičovou vatu, nebo minerální vlnu vyráběnou v Belgii</HSecondary>
+              <div
+                className='text-primary-100 text-lg flex flex-col gap-4'
+              >
+                <p>
+                  Foukaná minerální izolace je tepelná izolace, která se aplikuje na stěny, stropy a podhledy pomocí speciálního stroje, který rozptýlí vláknité materiály, jako jsou skleněné vlákna nebo minerální vlákna, do nástěnných prostorů. Foukananá izolace se díky svým vlastnostem poskytuje dokonalou tepelnou izolaci téměř každému objektu bez vzniku tepelných mostů. Tato izolace má několik výhod, jako jsou:
+                </p>
+                <p>
+                  1. Vysoká tepelná izolace: foukaná minerální izolace poskytuje vysokou tepelnou izolaci, což pomáhá udržovat teplo uvnitř budovy a snižuje potřebu vytápění.
+                </p>
+                <p>
+                  2. Snadná instalace: foukaná minerální izolace se snadno aplikuje pomocí speciálního stroje, což umožňuje rychlou a efektivní instalaci bez potřeby velkých stavebních úprav.
+                </p>
+                <p>
+                  3. Flexibilita: Foukaná minerální izolace se dá aplikovat na různé typy konstrukcí a prostorů, včetně obtížně přístupných míst.
+                </p>
+                <p>
+                  4. Odolnost: Foukaná minerální izolace je odolná proti vlhkosti a plísním, což ji činí vhodnou pro použití ve vlhkých prostorách, jako jsou koupelny nebo sklepy.
+                </p>
+                <p>
+                  5. Bezpečnost: Foukaná minerální izolace je nehořlavá, bezpečná pro použití v interiéru a neobsahuje žádné škodlivé látky.
+                </p>
+              </div>
+            </div>
           </div>
         </div>
 
         <img
           src='/business/ursa.jpg'
           alt='ursa'
-          className='flex-1 object-left object-cover w-auto h-[200px] xl:h-auto xl:w-[300px] order-1 xl:order-2'
+          className='hidden 2xl:block object-left object-cover w-[400px]'
         />
       </div>
+
     </SectionWrapper>
   );
 };

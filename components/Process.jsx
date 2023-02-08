18b47305c098441634797ad73b2e8454036ed9cc
Ondrej Juda <ondrej.juda@networg.com>
2023-02-08 22:28:30 +0100

    change photo

--- components/Process.jsx
+++ components/Process.jsx
@@ -5,7 +5,7 @@ const processSteps = [
   { image: '/business/first-contact.jpg', title: 'Osobní návštěva – konzultace', descripton: 'Náš odborný konzultant zaměří Vaši nemovitost a posoudí vhodnost použitého materiálu, tloušťku izolace a způsob provedení. Detailně Vám vysvětlí proces zakázky a průběh dotace.' },
   { image: '/business/architekt.jpg', title: 'Necháme zpracovat odborný posudek', descripton: 'Na základě nasbíraných dat necháme zpracovat odborný posudek potřebný k instalaci a schválení dotace.' },
   { image: '/business/documents.jpg', title: 'Zpracujeme a požádáme o dotaci z programu NZÚ', descripton: 'Pokud splňujete podmínky NZÚ Light, tak Vám může být dotace poskytnuta i před realizací' },
-  { image: '/business/roofworker.jpg', title: 'Realizace', descripton: 'Doba trvání realizace záleží na mnoha faktorech, ale u rodinných domů je realizace zpravidla hotová během jediného dne' },
+  { image: '/business/isolation2.png', title: 'Realizace', descripton: 'Doba trvání realizace záleží na mnoha faktorech, ale u rodinných domů je realizace zpravidla hotová během jediného dne' },
   { image: '/business/fin.jpeg', title: 'Doložení realizace NZÚ', descripton: 'Po realizaci doložíme všechny potřebné dokumenty na NZÚ' },
 ];
 

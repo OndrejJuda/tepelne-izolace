6211e130e3ceafa4a8c72ee2f420656acece7244
Ondrej Juda <ondrej.juda@networg.com>
2023-02-21 08:39:26 +0100

    image after hero section

---
+++ components/ImageSection.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { SectionWrapper } from './';
+
+const ImageSection = ({ sectionName, imageUrl }) => {
+  return (
+    <SectionWrapper
+      id={sectionName}
+      innerDivStyle='bg-secondary-600 lg:flex rounded-lg overflow-hidden shadow-xl mx-8 md:mx-16 lg:mx-36 w-full'
+    >
+      <img
+        src={imageUrl}
+        alt={sectionName}
+        className='w-full h-[600px] lg:h-full object-cover'
+      />
+    </SectionWrapper>
+  );
+};
+
+export default ImageSection;

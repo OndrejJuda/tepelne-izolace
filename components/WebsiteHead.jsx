bf53acad6e4c6e8850c0b17ed78a7dbc082f50e0
Ondrej Juda <ondrej.juda@networg.com>
2023-03-04 11:06:25 +0100

    google analytics

--- components/WebsiteHead.jsx
+++ components/WebsiteHead.jsx
@@ -1,49 +1,71 @@
 import React from 'react';
 import Head from 'next/head';
 import configuration from '../conf';
+import Script from 'next/script';
 
 const { title, description, url, jsonLd } = configuration;
+const measurementId = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS;
 
 const WebsiteHead = ({ titleSuffix, canonicalHref }) => {
   return (
-    <Head>
-      <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"></meta>
-      <link rel='canonical' href={canonicalHref}></link>
-      <meta name='viewport' content='width=device-width, initial-scale=1' />
-      <link rel='icon' href='/favicon/favicon.ico' />
-      <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
-      <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
-      <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
-      <link rel="manifest" href="/favicon/site.webmanifest" />
-      <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#e5a832" />
-      <meta name="msapplication-TileColor" content="#ffffff" />
-      <meta name="theme-color" content="#ffffff" />
+    <>
+      <Head>
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"></meta>
+        <link rel='canonical' href={canonicalHref}></link>
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <link rel='icon' href='/favicon/favicon.ico' />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
+        <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#e5a832" />
+        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta name="theme-color" content="#ffffff" />
 
-      {/* <!-- Primary Meta Tags --> */}
-      <title>{`${title}${titleSuffix ? titleSuffix : ''}`}</title>
-      <meta name='title' content={title} />
-      <meta name='description' content={description} />
+        {/* <!-- Primary Meta Tags --> */}
+        <title>{`${title}${titleSuffix ? titleSuffix : ''}`}</title>
+        <meta name='title' content={title} />
+        <meta name='description' content={description} />
 
-      {/* <!-- Open Graph / Facebook --> */}
-      <meta property='og:type' content='website' />
-      <meta property='og:url' content={url} />
-      <meta property='og:title' content={title} />
-      <meta property='og:description' content={description} />
-      <meta property='og:image' content='/business/metaimage.png' />
+        {/* <!-- Open Graph / Facebook --> */}
+        <meta property='og:type' content='website' />
+        <meta property='og:url' content={url} />
+        <meta property='og:title' content={title} />
+        <meta property='og:description' content={description} />
+        <meta property='og:image' content='/business/metaimage.png' />
 
-      {/* <!-- Twitter --> */}
-      <meta property='twitter:card' content='summary_large_image' />
-      <meta property='twitter:url' content={url} />
-      <meta property='twitter:title' content={title} />
-      <meta property='twitter:description' content={description} />
-      <meta property='twitter:image' content='/business/metaimage.png'></meta>
+        {/* <!-- Twitter --> */}
+        <meta property='twitter:card' content='summary_large_image' />
+        <meta property='twitter:url' content={url} />
+        <meta property='twitter:title' content={title} />
+        <meta property='twitter:description' content={description} />
+        <meta property='twitter:image' content='/business/metaimage.png'></meta>
 
-      {/* Schema Markup JSON-LD */}
-      <script
-        type="application/ld+json"
-        dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
-      />
-    </Head>
+        {/* Schema Markup JSON-LD */}
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+        />
+      </Head>
+      {
+        measurementId && (
+          <>
+            <Script
+              strategy="afterInteractive"
+              src={`https://www.googletagmanager.com/gtag/js?id=${measurementId}`}
+            />
+            <Script id="google-analytics" strategy="afterInteractive">
+              {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${measurementId}');
+              `}
+            </Script>
+          </>
+        )
+      }
+    </>
   );
 };
 

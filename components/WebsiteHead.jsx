4b2252753006d8f59beb4a50cf1fd999df6ee6d6
Roman Erlebach <roman.erle@seznam.cz>
2023-11-27 11:08:08 +0100

    added innethtml

--- components/WebsiteHead.jsx
+++ components/WebsiteHead.jsx
@@ -43,22 +43,35 @@ const WebsiteHead = ({ titleSuffix, canonicalHref }) => {
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
         />
-        <script async dangerouslySetInnerHTML={{
+
+        <script dangerouslySetInnerHTML={{
           __html: `
-        !function(f,b,e,v,n,t,s)
-        {if(f.fbq)return;n=f.fbq=function(){n.callMethod ?
-          n.callMethod.apply(n, arguments) : n.queue.push(arguments)};
-        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-        n.queue=[];t=b.createElement(e);t.async=!0;
-        t.src=v;s=b.getElementsByTagName(e)[0];
-s.parentNode.insertBefore(t,s)}(window, document,'script',
-        'https://connect.facebook.net/en_US/fbevents.js');
-        fbq('init', '1747459502334265');
-        fbq('track', 'PageView');
-      `}} />
-        <noscript><img height="1" width="1" style="display:none"
-          src="https://www.facebook.com/tr?id=1747459502334265&ev=PageView&noscript=1"
-        /></noscript>
+    !function(f,b,e,v,n,t,s) {
+      if(f.fbq) return;
+      n=f.fbq=function(){n.callMethod ?
+        n.callMethod.apply(n, arguments) : n.queue.push(arguments)};
+      if(!f._fbq) f._fbq=n;
+      n.push=n;
+      n.loaded=!0;
+      n.version='2.0';
+      n.queue=[];
+      t=b.createElement(e);
+      t.async=!0;
+      t.src=v;
+      s=b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t,s)
+    }(window, document, 'script',
+    'https://connect.facebook.net/en_US/fbevents.js');
+    fbq('init', '1747459502334265');
+    fbq('track', 'PageView');
+  ` }} />
+
+        <noscript dangerouslySetInnerHTML={{
+          __html: `
+    <img height="1" width="1" style="display:none"
+      src="https://www.facebook.com/tr?id=1747459502334265&ev=PageView&noscript=1"
+    />
+  ` }} />
       </Head>
     </>
   );

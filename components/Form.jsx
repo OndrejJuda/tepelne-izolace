0da4ecf238a02500b7cbdc9ef18e032113671fc9
Roman Erlebach <roman.erle@seznam.cz>
2023-11-30 14:25:49 +0100

    test

--- components/Form.jsx
+++ components/Form.jsx
@@ -165,7 +165,6 @@ const Form = () => {
 
           const getIp = await fetch('/api/get-ip');
           const ipAddress = await getIp.json();
-          console.log(data.ip)
           const fb = await fetch(
             '/api/facebook/',
             {

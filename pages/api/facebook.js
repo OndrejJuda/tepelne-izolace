93bb9eb7eaa57cac8b74b72950742323d29fa475
Roman Erlebach <roman.erle@seznam.cz>
2023-11-30 13:20:36 +0100

    lead id deleted

--- pages/api/facebook.js
+++ pages/api/facebook.js
@@ -13,13 +13,12 @@ const sendFBdata = async (req, res) => {
 
   const myHeaders = new Headers();
   myHeaders.append("Content-Type", "application/json; charset=UTF-8");
-  const leadId = Math.floor(Math.random() * (999999999 - 100000000 + 1) + 100000000);
   const hashEmail = sha256(email.toLowerCase());
   const hashfirstName = sha256(firstName.toLowerCase());
   const hashlastName = sha256(lastName.toLowerCase());
   const hashphoneNumber = sha256(phoneNumber.toLowerCase());
   const hashdistrict = sha256(district.toLowerCase());
-  const dataToSend = ` "{\r\n    \"data\": [\r\n        {\r\n            \"event_name\": \"Lead\",\r\n               \"user_data\": {\r\n                \"em\": [\r\n                    \"${hashEmail}\"\r\n                ],\r\n                \"ph\": [\r\n ${hashphoneNumber}\r\n                ],\r\n                \"ct\": [\r\n  ${hashdistrict}\r\n                ],\r\n                \"client_ip_address\": ${ipAddress},\r\n                         \"ln\": [\r\n ${hashlastName}\r\n                ],\r\n                \"lead_id\": ${leadId},\r\n                \"fn\": [\r\n ${hashfirstName}\r\n                ]\r\n            }\r\n        }\r\n    ]\r\n}";`;
+  const dataToSend = ` "{\r\n    \"data\": [\r\n        {\r\n            \"event_name\": \"Lead\",\r\n               \"user_data\": {\r\n                \"em\": [\r\n                    \"${hashEmail}\"\r\n                ],\r\n                \"ph\": [\r\n ${hashphoneNumber}\r\n                ],\r\n                \"ct\": [\r\n  ${hashdistrict}\r\n                ],\r\n                \"client_ip_address\": ${ipAddress},\r\n                         \"ln\": [\r\n ${hashlastName}\r\n                ],\r\n                     \"fn\": [\r\n ${hashfirstName}\r\n                ]\r\n            }\r\n        }\r\n    ]\r\n}";`;
 
   const requestOptions = {
     method: 'POST',

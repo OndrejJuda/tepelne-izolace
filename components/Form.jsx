90b67487868607e23a5b48c1deaaf18f3c4a0150
Roman Erlebach <roman.erle@seznam.cz>
2023-11-30 14:38:23 +0100

    date

--- components/Form.jsx
+++ components/Form.jsx
@@ -175,7 +175,8 @@ const Form = () => {
           const hashlastName = sha256(lastName.toLowerCase());
           const hashphoneNumber = sha256(phoneNumber.toLowerCase());
           const hashdistrict = sha256(district.toLowerCase());
-          const dataToSend = `{\r\n    \"data\": [\r\n        {\r\n            \"event_name\": \"Lead\",\r\n            \"event_time\": ${new Date().getTime()},\r\n            \"action_source\": \"website\",\r\n            \"user_data\": {\r\n                \"em\": [\r\n                    \"${hashEmail}\"\r\n                ],\r\n                \"ph\": [\r\n                    \"${hashphoneNumber}\"\r\n                ],\r\n                \"ct\": [\r\n                    \"${hashdistrict}\"\r\n                ],\r\n                \"client_ip_address\": \"${ipAddress}\",\r\n                \"ln\": [\r\n                    \"${hashlastName}\"\r\n                ],\r\n                \"fn\": [\r\n                    \"${hashfirstName}\"\r\n                ]\r\n            }\r\n        }\r\n    ]\r\n}`;
+          const date = new Date().getTime();
+          const dataToSend = `{\r\n    \"data\": [\r\n        {\r\n            \"event_name\": \"Lead\",\r\n            \"event_time\": ${date},\r\n            \"action_source\": \"website\",\r\n            \"user_data\": {\r\n                \"em\": [\r\n                    \"${hashEmail}\"\r\n                ],\r\n                \"ph\": [\r\n                    \"${hashphoneNumber}\"\r\n                ],\r\n                \"ct\": [\r\n                    \"${hashdistrict}\"\r\n                ],\r\n                \"client_ip_address\": \"${ipAddress}\",\r\n                \"ln\": [\r\n                    \"${hashlastName}\"\r\n                ],\r\n                \"fn\": [\r\n                    \"${hashfirstName}\"\r\n                ]\r\n            }\r\n        }\r\n    ]\r\n}`;
 
           const requestOptions = {
             method: 'POST',

b0d325e05ce7fbb5770cbe06c71016e513a5c8f3
Roman Erlebach <roman.erle@seznam.cz>
2023-12-04 12:24:54 +0100

    deleted react

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,5 +1,4 @@
 /* eslint-disable @next/next/no-sync-scripts */
-import React from 'react';
 import Link from 'next/link';
 import configuration from '../conf';
 import { FaFacebook, FaSolarPanel, FaInstagram } from "react-icons/fa";

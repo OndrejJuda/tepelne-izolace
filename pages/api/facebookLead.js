ca981a65ed7718166a75a0ae271a12cdd36ed83b
Roman Erlebach <roman.erle@seznam.cz>
2023-12-04 14:28:45 +0100

    missing crypto

--- pages/api/facebookLead.js
+++ pages/api/facebookLead.js
@@ -1,5 +1,6 @@
 const apiKey = process.env.FACEBOOK_TOKEN;
 const datasetID = process.env.FACEBOOK_PIXEL_ID;
+import { createHash } from 'crypto';
 
 const sha256 = (data) => {
   const hash = createHash('sha256');
@@ -13,45 +14,34 @@ const sendFBLeaddata = async (req, res) => {
 
   const myHeaders = new Headers();
   myHeaders.append("Content-Type", "application/json; charset=UTF-8");
-  // const guid = "e68cb608-4d6b-4ec4-ab9b-1310b790ec7a";
-  // const hashEmail = sha256(email.toLowerCase());
-  // const hashfirstName = sha256(firstName.toLowerCase());
-  // const hashlastName = sha256(lastName.toLowerCase());
-  // const hashphoneNumber = sha256(phoneNumber);
-  // const hashdistrict = sha256(district.name.toLowerCase());
-  // const date = Math.floor((new Date().getTime()) / 1000);
+  const guid = "e68cb608-4d6b-4ec4-ab9b-1310b790ec7a";
+  const hashEmail = sha256(email.toLowerCase());
+  const hashfirstName = sha256(firstName.toLowerCase());
+  const hashlastName = sha256(lastName.toLowerCase());
+  const hashphoneNumber = sha256(phoneNumber);
+  const hashdistrict = sha256(district.name.toLowerCase());
+  const date = Math.floor((new Date().getTime()) / 1000);
 
-  // const dataToSend = JSON.stringify({
-  //   "data": [
-  //     {
-  //       "action_source": "website",
-  //       "event_name": "Lead",
-  //       "event_id": guid,
-  //       "event_time": date,
-  //       "event_source_url": currentUrl,
-  //       "user_data": {
-  //         "em": hashEmail,
-  //         "ln": hashlastName,
-  //         "client_ip_address": ipAddress,
-  //         "fn": hashfirstName,
-  //         "ph": hashphoneNumber,
-  //         "ct": hashdistrict
-  //       }
-  //     }
-  //   ]
-  // })
   const dataToSend = JSON.stringify({
     "data": [
       {
         "action_source": "website",
         "event_name": "Lead",
-        "event_time": "1701343587",
+        "event_id": guid,
+        "event_time": date,
+        "event_source_url": currentUrl,
         "user_data": {
-          "em": "7b17fb0bd173f625b58636fb796407c22b3d16fc78302d79f0fd30c2fc2fc068",
+          "em": hashEmail,
+          "ln": hashlastName,
+          "client_ip_address": ipAddress,
+          "fn": hashfirstName,
+          "ph": hashphoneNumber,
+          "ct": hashdistrict
         }
       }
     ]
   })
+
 
 
   const requestOptions = {

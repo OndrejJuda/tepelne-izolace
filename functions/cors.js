4b7066c90fab1bdb273d3242baf0b67f0a6520c1
Roman Erlebach <roman.erle@seznam.cz>
2024-01-03 15:09:28 +0100

    updated function

--- functions/cors.js
+++ functions/cors.js
@@ -1,11 +1,17 @@
 exports.handler = async function (event, context) {
-    return {
-        statusCode: 200,
+    const apiUrl = 'https://lunastav.cz/api/raynet';
+
+    const response = await fetch(apiUrl, {
+        method: event.httpMethod,
         headers: {
-            'Access-Control-Allow-Origin': 'https://www.damepanely.cz',
-            'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
-            'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',  // Adjust as needed
         },
-        body: JSON.stringify({ message: 'CORS enabled for https://www.damepanely.cz/' }),
+        body: event.body,
+    });
+
+    return {
+        statusCode: response.status,
+        body: JSON.stringify(await response.json()),
     };
 };

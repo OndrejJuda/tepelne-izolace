58c6880867b8b14955f65fb6ea3713451e877890
Roman Erlebach <roman.erle@seznam.cz>
2024-01-03 11:32:46 +0100

    fix cors

--- next.config.js
+++ next.config.js
@@ -10,17 +10,26 @@ const nextConfig = {
     return config;
   },
   async headers() {
-    return [
-      {
-        source: "/api/raynet",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "https://www.damepanely.cz" },
-          { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
-          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-        ]
-      }
-    ];
+    return {
+      "/api/raynet": [
+        {
+          key: "Access-Control-Allow-Credentials",
+          value: "true",
+        },
+        {
+          key: "Access-Control-Allow-Origin",
+          value: "https://www.damepanely.cz",
+        },
+        {
+          key: "Access-Control-Allow-Methods",
+          value: "GET,DELETE,PATCH,POST,PUT",
+        },
+        {
+          key: "Access-Control-Allow-Headers",
+          value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+        },
+      ],
+    };
   }
 };
 

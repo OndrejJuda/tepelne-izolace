0b85220b161adbcf099ff21570bfeafdac98c5dd
Roman Erlebach <roman.erle@seznam.cz>
2024-01-03 14:00:04 +0100

    test

--- next.config.js
+++ next.config.js
@@ -1,29 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-
-  experimental: {
-    appDir: true,
-    async headers() {
-      return [
-        {
-          // matching all API routes
-          source: "/api/:path*",
-          headers: [
-            // omitted for brevity...
-          ]
-        },
-        {
-          source: "/api/raynet",
-          headers: [
-            { key: "Access-Control-Allow-Credentials", value: "false" },
-            { key: "Access-Control-Allow-Origin", value: "https://damepanely.com" },
-            { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
-            { key: "Access-Control-Allow-Headers", value: "Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date" },
-          ]
-        }
-      ]
-    }
+  async headers() {
+    return [
+      {
+        source: "/api/raynet",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "false" },
+          { key: "Access-Control-Allow-Origin", value: "https://damepanely.com" },
+          { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date" },
+        ]
+      }
+    ]
   },
 
   webpack: (

cb3a2c7c16a3b7a8266c34556874ff92a7231921
Roman Erlebach <roman.erle@seznam.cz>
2024-01-03 13:47:28 +0100

    test

--- next.config.js
+++ next.config.js
@@ -7,23 +7,14 @@ const nextConfig = {
     async headers() {
       return [
         {
-          // Matching all API routes
           source: "/api/:path*",
           headers: [
             { key: "Access-Control-Allow-Credentials", value: "true" },
-            { key: "Access-Control-Allow-Origin", value: "https://damepanely.cz https://lunastav.cz" },
-            {
-              key: "Access-Control-Allow-Methods",
-              value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
-            },
-            {
-              key: "Access-Control-Allow-Headers",
-              value:
-                "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-            },
+            { key: "Access-Control-Allow-Origin", value: "https://damepanely.cz" },
+            { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+            { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
           ],
         },
-        // Additional headers configurations for other routes if needed
       ];
     },
   },
@@ -32,7 +23,6 @@ const nextConfig = {
     config,
     { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
   ) => {
-    // Important: return the modified config
     config.resolve.extensions.push('.web.js');
     return config;
   },

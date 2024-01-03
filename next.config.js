1198a0bc6401629564d9173814211f1bfe2cb418
Roman Erlebach <roman.erle@seznam.cz>
2024-01-03 12:35:05 +0100

    test cors

--- next.config.js
+++ next.config.js
@@ -10,27 +10,20 @@ const nextConfig = {
     return config;
   },
   async headers() {
-    return {
-      "/api/raynet": [
-        {
-          key: "Access-Control-Allow-Credentials",
-          value: "true",
-        },
-        {
-          key: "Access-Control-Allow-Origin",
-          value: "https://www.damepanely.cz",
-        },
-        {
-          key: "Access-Control-Allow-Methods",
-          value: "GET,DELETE,PATCH,POST,PUT",
-        },
-        {
-          key: "Access-Control-Allow-Headers",
-          value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-        },
-      ],
-    };
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "https://www.damepanely.cz/" }, // replace this your actual origin
+          { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
+    ]
   }
+
 };
 
 module.exports = nextConfig;

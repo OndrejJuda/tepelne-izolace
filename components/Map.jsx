9bfe7b3176ed7b7ab525bb05efe2d9dd0850c8bf
Roman Erlebach <roman.erle@seznam.cz>
2024-02-20 10:15:48 +0100

    map update

--- components/Map.jsx
+++ components/Map.jsx
@@ -2,14 +2,14 @@ import { Map, Marker, GoogleApiWrapper } from 'google-maps-react';
 
 const PLACES = [
   { lat: 50.08025330059694, lng: 14.414347861165416 },
-  { lat: 50.21311550155654, lng: 15.836227860702156 }
+  { lat: 50.22383794575171, lng: 15.78019676441785 }
 ];
 
 const MapContainer = ({ google }) => {
   return (
     <Map
       google={google}
-      zoom={7}
+      zoom={9}
       initialCenter={{ lat: 49.763318, lng: 15.804715 }}
     >
       {

5551c97cfdf855189312b9a06e27dab1a3ca066b
Roman Erlebach <roman.erle@seznam.cz>
2024-02-21 13:51:37 +0100

    dotace update

--- components/Grant.jsx
+++ components/Grant.jsx
@@ -3,6 +3,10 @@ import { SectionWrapper } from './';
 import { Header, Breadcrumb } from '../components';
 import { FiHome, FiChevronRight } from 'react-icons/fi';
 import Link from 'next/link';
+import { FaSolarPanel, FaWarehouse } from "react-icons/fa";
+import { MdOutlineRoofing } from "react-icons/md";
+import { SiHiveBlockchain } from "react-icons/si";
+import { BsArrowBarDown } from "react-icons/bs";
 
 const Grant = () => {
   const [isVisible, setIsVisible] = useState(true);
@@ -41,31 +45,80 @@ const Grant = () => {
   return (
     <>
       <Header />
-      <SectionWrapper innerDivStyle="my-14 md:my-14">
-        <div className='flex flex-col 2xl:flex-row 2xl:items-center px-4 md:px-16 lg:px-36 gap-16 2xl:gap-32'>
-          <div className='flex flex-col gap-8 text-primary-900 order-2 2xl:order-1'>
-            <Breadcrumb items={breadcrumbItems} />
-            <h1 className='text-4xl font-semibold'>Vyřizujeme dotace z programu NZÚ Light</h1>
-            <p className='text-lg'>
-              Se zvládnutím energetické krize nám má pomoci nový dotační program s názvem Nová zelená úsporám Light (NZÚ Light). V rámci tohoto programu můžete čerpat dotace na zateplení různých částí domu jako je strop, střecha či fasáda, nebo na výměnu plastových oken a vchodových dveří. Výhodou celého programu je rychlost čerpání dotace a pomoc tam, kde je to skutečně potřeba a také možnost čerpání i v případech, že už je některá část domu zateplená. Například pokud máte zateplené podkroví, tak můžete čerpat dotaci na zateplení fasády a opačně.
-            </p>
-            <p className='text-lg'>
-              Pokud jste v důchodovém věku, nebo invalidním důchodu, tak Vám může dotace pokrýt až 100% z celkové ceny zateplení. Peníze na zateplení můžete čerpat i předem. Program je také určen pro vlastníky a spoluvlastníky rodinných domů z řad seniorů a lidí pobírajících dávky na bydlení. Podmínkou je, aby v době podání žádosti byl rodinný dům ve vlastnictví žadatelů a ti v něm měli současně trvalý pobyt, a to déle než od 12. září 2022.
-            </p>
-            <p className='text-lg'>
-              U klasické Nové zelené úsporám, se příspěvek vyplácí až po realizaci opatření. NZÚ Light na to jde z opačné strany a příspěvek můžete obdržet ještě před samotnou realizací. Po schválení dotace jsou Vám vyplaceny finance, za které můžete zateplení či výměnu oken provést. Díky tomu se jedná o zcela unikátní projekt, jaký tu ještě nebyl.
-            </p>
+      <SectionWrapper
+        id='dotace'
+        innerDivStyle='flex flex-col justify-center bg-white rounded-lg mx-8 md:mx-16 lg:mx-36 w-screen my-10 relative'
+      >
+        <Breadcrumb items={breadcrumbItems} />
+
+        <div className="bg-white py-10 sm:py-10">
+          <div className="mx-auto max-w-7xl px-6 lg:px-8">
+            <div className="mx-auto max-w-2xl lg:text-center">
+              <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Dotaci NZÚ Light vyřídíme my!</p>
+              <p className="mt-4 text-lg leading-8 text-gray-600">Podáme za Vás žádost o tyto dotace a vyřídíme veškerou administrativu.</p>
+            </div>
+            <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-16 lg:max-w-4xl">
+              <dl className="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-16">
+                <div className="relative pl-16">
+                  <dt className="text-base font-semibold leading-7 text-gray-900">
+                    <div className="absolute left-0 top-0 flex h-12 w-12 items-center justify-center rounded-lg bg-primary-300">
+                      <BsArrowBarDown className='h-6 w-6' />
+                    </div>
+                    Zateplení stropu
+                  </dt>
+                  <dd className="mt-2 mb-2  text-base leading-7 text-gray-600">Dotace na kompletní zateplení stropu pod nevytápěnými prostory nebo stropu obývaného podkroví.</dd>
+                  <Link href="/dotace" title="Zateplení stropu" className="mt-4 text-sm font-medium text-primary-500 hover:text-primary-600">
+                    Zjistit více
+                  </Link>
+                </div>
+                <div className="relative pl-16">
+                  <dt className="text-base font-semibold leading-7 text-gray-900">
+                    <div className="absolute left-0 top-0 flex h-12 w-12 items-center justify-center rounded-lg bg-primary-300">
+                      <MdOutlineRoofing className='h-8 w-8 ' />
+                    </div>
+                    Zateplení střechy
+                  </dt>
+                  <dd className="mt-2 mb-2  text-base leading-7 text-gray-600">Dotace na kompletní zateplení střechy.</dd>
+                  <Link href="/dotace" title="Zateplení stropu" className="mt-4 text-sm font-medium text-primary-500 hover:text-primary-600">
+                    Zjistit více
+                  </Link>
+                </div>
+                <div className="relative pl-16">
+                  <dt className="text-base font-semibold leading-7 text-gray-900">
+                    <div className="absolute left-0 top-0 flex h-12 w-12 items-center justify-center rounded-lg bg-primary-300">
+                      <SiHiveBlockchain className='h-6 w-6' />
+                    </div>
+                    Zateplení fasády
+                  </dt>
+                  <dd className="mt-2 mb-2  text-base leading-7 text-gray-600">Dotace na zateplení fasády vhodnými tepelněizolačními materiály.</dd>
+                  <Link href="/dotace" title="Zateplení stropu" className="mt-4 text-sm font-medium text-primary-500 hover:text-primary-600">
+                    Zjistit více
+                  </Link>
+                </div>
+                <div className="relative pl-16">
+                  <dt className="text-base font-semibold leading-7 text-gray-900">
+                    <div className="absolute left-0 top-0 flex h-12 w-12 items-center justify-center rounded-lg bg-primary-300">
+                      <FaSolarPanel className='h-6 w-6' />
+                    </div>
+                    FVE na ohřev vody
+                  </dt>
+                  <dd className="mt-2 mb-2 text-base leading-7 text-gray-600">Dotace na termický nebo fotovoltaický ohřev vody s využitím stávajícího bojleru nebo s novým bojlerem.</dd>
+                  <Link href="/dotace" title="Zateplení stropu" className="text-sm font-medium text-primary-500 hover:text-primary-600">
+                    Zjistit více
+                  </Link>
+                </div>
+              </dl>
+            </div>
+            <div>
+              <ul className="list-disc mt-16 text-lg">
+                <strong className='text-2xl '>Podmínky pro získání dotace:</strong>
+                <li className='mb-2 mt-2'>Jste vlastníci nebo spoluvlastníci rodinného domu, nebo trvale obývané rekreační stavby, bytu nebo podílu v bytovém družstvu a máte tam trvalý pobyt déle než od 12. září 2022.</li>
+                <li className='mb-2 '>Jsou všichni členové vaší domácnosti příjemci starobního důchodu nebo invalidního důchodu 3. stupně, nebo vaše domácnost pobírala v období od 12. září 2022 příspěvek na bydlení či přídavky na děti aspoň jeden měsíc.</li>
+              </ul>
+            </div>
           </div>
         </div>
-        <div className="bg-white p-8 md:p-12 lg:px-10 lg:py-12 my-16 rounded-lg">
-          <div className="mb-10 text-4xl font-bold text-center">
-            Jsme specialisté <span className="text-primary-nzumodra">Nové</span> <span className="text-primary-nzuzelena">zelené</span> <span className="text-primary-nzuzluta">úsporám</span>
-          </div>
-          <Link href='https://novazelenausporam.cz/specialista/14368' className='flex justify-center items-center w-full font-bold scale-100 hover:scale-110 duration-200 '>
-            <div className="text-">Oficialní seznam specialistů</div>
-          </Link>
-        </div>
-      </SectionWrapper>
+      </SectionWrapper >
     </>
   );
 };

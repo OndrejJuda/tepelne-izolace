fe24b41e5f5c7489308ead86e7878b4b986706e1
Roman Erlebach <roman.erle@seznam.cz>
2024-07-03 11:49:40 +0200

    gelerie update

--- components/Gallery.jsx
+++ components/Gallery.jsx
@@ -1,34 +1,28 @@
 import React, { useEffect, useState } from 'react';
 import { HSecondary, SectionWrapper } from './';
 import { useWindowWidth } from '@react-hook/window-size';
+import { Carousel } from '../components';
 
 const Gallery = () => {
-  const useWidth = useWindowWidth();
-  const [sizeClassName, setSizeClassName] = useState(undefined);
 
-  useEffect(() => {
+  const blowingInsualtionSlides = [
+    "/gallery/lavka1.jpg",
+    "/gallery/vata1.jpg",
+    "/gallery/vata2.jpg",
+    "/gallery/celuloza1.jpg",
+    "/gallery/celuloza2.jpg",
 
-    if (useWidth > 1000) {
-      setSizeClassName('big');
-    } else if (useWidth < 600) {
-      setSizeClassName('small');
-    } else {
-      setSizeClassName('medium');
-    }
-  }, [useWidth]);
-
+  ]
   return (
     <SectionWrapper
       id='galerie'
       innerDivStyle='bg-primary-50 my-12 md:my-16 w-screen p-8 overflow-x-hidden flex flex-col justify-center items-center px-8 md:px-16 lg:px-36'
     >
-      <HSecondary additionalStyling='mb-8 lg:mb-16 mt-8 text-2xl sm:text-2xl lg:text-3xl'>Galerie</HSecondary>
-      <div className={`gallery__woxo-carousel--${sizeClassName}`}>
-        <div data-mc-src="2321a380-7adb-403f-a172-a9103affccf3#instagram"></div>
-        <script
-          src="https://cdn2.woxo.tech/a.js#65cdfc5f13d9d3047ad36ff6"
-          async data-usrc>
-        </script>
+      <HSecondary additionalStyling='mb-8 lg:mb-8 mt-8 text-2xl sm:text-2xl lg:text-3xl'>Galerie</HSecondary>
+      <div className="flex justify-center mb-10">
+        <div className="max-w-[800px]">
+          <Carousel slides={blowingInsualtionSlides} />
+        </div>
       </div>
     </SectionWrapper>
   );
